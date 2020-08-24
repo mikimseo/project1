25f1a6f43b2f975a315b71463499e0768888e28c
mikimseo <kim@sgc.kz>
2020-08-24 13:09:26 +0600

    lesson 016

---
+++ js/script016.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
+
+showFirstMessage("Hello World!");
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret ();
+console.log(anotherNum);
+
+// function expression 
+const logger = function() {
+    console.log('Hello');
+};
+
+logger();
+//end
+
+//стрелочная функция
+const calc = (a, b) => { 
+    console.log('1');
+    return a + b;
+};
