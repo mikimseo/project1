528f94013bf1c8dae8f6496e19516a0f6d4eae16
mikimseo <kim@sgc.kz>
2020-08-28 01:07:59 +0600

    получаем количество ключей в объекте

--- js/script020.js
+++ js/script020.js
@@ -11,17 +11,23 @@ const options = {
     }
 }
 
+console.log(Object.keys(options).length);
+
 // console.log(options["colors"]["border"]);
 
 // delete options.name;
 // console.log(options);
 
-for (let key in options) {
-    if (typeof(options[key]) === 'object') {
-        for (let i in options[key]) {
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-        }
-    } else {
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-    }
-}
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter ++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter ++;
+//     }
+// }
+// console.log(counter);
