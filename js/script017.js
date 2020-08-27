2c63a4112c07b0feaa19c2b5631004797907612c
mikimseo <kim@sgc.kz>
2020-08-27 23:40:39 +0600

    lesson017

---
+++ js/script017.js
@@ -0,0 +1,35 @@
+"use strict"
+
+// const str = "test";
+// const arr = [1, 2, 4];
+
+// console.log(str.length);
+// console.log(arr.length);
+
+// console.log(str[2]);
+
+const str = "Test";
+
+//console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello World!";
+console.log(logg.slice(6, 12));
+
+const logg = "1234567890";
+console.log(logg.slice(3, 9));
+
+const logg = "Hello World!";
+console.log(logg.substring(6, 12));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
